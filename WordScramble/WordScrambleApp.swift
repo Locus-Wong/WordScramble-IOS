d7f02bfeee73a24ff03ea1769a51f463c3a84397
TSZ WA WONG <twwong9@myseneca.ca>
2025-07-09 18:32:06 -0400

    Add Project Source Code

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Locus Wong on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
